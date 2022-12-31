698159556beca7b2ee1830c63c3fafc76d2fd653
Ilyas Kazi <ilyaskazi@gmail.com>
2022-12-31 19:34:34 +0530

    Removed starter template trails

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,28 @@ const Home = () => {
   const pageDescription = 'welcome to react voting app'
 
   return (
+    <>
     <div>
       <Meta title={pageTitle} description={pageDescription} />
       <Header head={pageTitle} description={pageDescription} />
     </div>
+    <div class="poll-container">
+  <h2 class="poll-question">What is your favorite color?</h2>
+  <form class="poll-form">
+    <label>
+      <input type="radio" name="color" value="red" /> Red
+    </label>
+    <label>
+      <input type="radio" name="color" value="blue" /> Blue
+    </label>
+    <label>
+      <input type="radio" name="color" value="green" /> Green
+    </label>
+    <input type="submit" value="Submit" />
+  </form>
+</div>
+
+    </>
   )
 }
 
